765b6c2293e5f679d79cf6c404c2eae4d404d673
EddieToledoE <teddy_03@outlook.es>
2024-12-02 18:29:10 -0600

    Cambios para simular datos

--- mqqt-ws.js
+++ mqqt-ws.js
@@ -1,21 +1,26 @@
-const mqtt = require('mqtt');
-const http = require('http');
-const socketIo = require('socket.io');
+const mqtt = require("mqtt");
+const http = require("http");
+const socketIo = require("socket.io");
 
 // Configuración del broker MQTT de CloudAMQP
-const brokerUrl = "mqtt://toad.rmq.cloudamqp.com";
+const brokerUrl = "mqtt://moose.rmq.cloudamqp.com";
 const brokerPort = 1883;
 
 // Credenciales MQTT
-const username = "jugilxxo:jugilxxo";
-const password = "aqvvDO1Y0hq2iW03wmz08TONcWdov1z0";
+const username = "iqyrnjwl:iqyrnjwl";
+const password = "rQP4_WhDq4ZrynLBTHzbQrD3hG1TrcND";
 
 // Temas MQTT al que suscribirse
-const sensorTopic = "hydrop/668dee66cf7b5b0a30fb22a4/6699defcf167387f3335e144/sensor_data";
-const pumpStatusTopic = "hydrop/668dee66cf7b5b0a30fb22a4/6699defcf167387f3335e144/pump_status";
-const nutrientDispenserStatusTopic = "hydrop/668dee66cf7b5b0a30fb22a4/6699defcf167387f3335e144/nutrient_dispenser_status";
-const pumpCommandTopic = "hydrop/668dee66cf7b5b0a30fb22a4/6699defcf167387f3335e144/pump/command";
-const nutrientDispenserCommandTopic = "hydrop/668dee66cf7b5b0a30fb22a4/6699defcf167387f3335e144/nutrient_dispenser/command";
+const sensorTopic =
+  "hydrop/668dee66cf7b5b0a30fb22a4/6699defcf167387f3335e144/sensor_data";
+const pumpStatusTopic =
+  "hydrop/668dee66cf7b5b0a30fb22a4/6699defcf167387f3335e144/pump_status";
+const nutrientDispenserStatusTopic =
+  "hydrop/668dee66cf7b5b0a30fb22a4/6699defcf167387f3335e144/nutrient_dispenser_status";
+const pumpCommandTopic =
+  "hydrop/668dee66cf7b5b0a30fb22a4/6699defcf167387f3335e144/pump/command";
+const nutrientDispenserCommandTopic =
+  "hydrop/668dee66cf7b5b0a30fb22a4/6699defcf167387f3335e144/nutrient_dispenser/command";
 
 // Crear cliente MQTT
 const client = mqtt.connect(brokerUrl, {
@@ -29,76 +34,97 @@ const server = http.createServer();
 const io = socketIo(server, {
   cors: {
     origin: "http://localhost:3000",
-    methods: ["GET", "POST"]
-  }
+    methods: ["GET", "POST"],
+    credentials: true, // Permitir credenciales (cookies, autenticación, etc.)
+  },
 });
 
 // Manejar conexión de cliente Socket.io
-io.on('connection', (socket) => {
-  console.log('Cliente WebSocket conectado');
+io.on("connection", (socket) => {
+  console.log("Cliente WebSocket conectado");
 
-  socket.on('join_station', (stationId) => {
+  socket.on("join_station", (stationId) => {
     console.log(`Cliente unido a la estación: ${stationId}`);
     socket.join(stationId); // Unir al cliente a la sala de la estación
   });
 
-  socket.on('pump_command', (command) => {
+  socket.on("pump_command", (command) => {
     console.log(`Comando de bomba recibido: ${JSON.stringify(command)}`);
     client.publish(pumpCommandTopic, JSON.stringify(command), (err) => {
       if (err) {
-        console.error('Error al publicar comando de bomba:', err);
+        console.error("Error al publicar comando de bomba:", err);
       } else {
-        console.log('Comando de bomba publicado:', command);
+        console.log("Comando de bomba publicado:", command);
       }
     });
   });
 
-  socket.on('nutrient_dispenser_command', (command) => {
-    console.log(`Comando de dispensador de nutrientes recibido: ${JSON.stringify(command)}`);
-    client.publish(nutrientDispenserCommandTopic, JSON.stringify(command), (err) => {
-      if (err) {
-        console.error('Error al publicar comando de dispensador de nutrientes:', err);
-      } else {
-        console.log('Comando de dispensador de nutrientes publicado:', command);
+  socket.on("nutrient_dispenser_command", (command) => {
+    console.log(
+      `Comando de dispensador de nutrientes recibido: ${JSON.stringify(
+        command
+      )}`
+    );
+    client.publish(
+      nutrientDispenserCommandTopic,
+      JSON.stringify(command),
+      (err) => {
+        if (err) {
+          console.error(
+            "Error al publicar comando de dispensador de nutrientes:",
+            err
+          );
+        } else {
+          console.log(
+            "Comando de dispensador de nutrientes publicado:",
+            command
+          );
+        }
       }
-    });
+    );
   });
 
-  socket.on('disconnect', () => {
-    console.log('Cliente WebSocket desconectado');
+  socket.on("disconnect", () => {
+    console.log("Cliente WebSocket desconectado");
   });
 });
 
 // Manejar conexión MQTT
-client.on('connect', () => {
+client.on("connect", () => {
   console.log("Conectado al broker MQTT!");
-  client.subscribe([sensorTopic, pumpStatusTopic, nutrientDispenserStatusTopic], (err) => {
-    if (err) {
-      console.error(`Error al suscribirse al tema: ${err}`);
+  client.subscribe(
+    [sensorTopic, pumpStatusTopic, nutrientDispenserStatusTopic],
+    (err) => {
+      if (err) {
+        console.error(`Error al suscribirse al tema: ${err}`);
+      }
     }
-  });
+  );
 });
 
 // Manejar mensajes MQTT
-client.on('message', (topic, message) => {
-  const stationId = topic.split('/')[2]; // Extraer el ID de la estación del tema
+client.on("message", (topic, message) => {
+  const stationId = topic.split("/")[2]; // Extraer el ID de la estación del tema
   console.log(`Mensaje recibido en el tema ${topic}: ${message.toString()}`);
 
   if (topic === sensorTopic) {
-    io.to(stationId).emit('sensor_data', JSON.parse(message.toString()));
+    io.to(stationId).emit("sensor_data", JSON.parse(message.toString()));
   } else if (topic === pumpStatusTopic) {
-    io.to(stationId).emit('pump_status', JSON.parse(message.toString()));
+    io.to(stationId).emit("pump_status", JSON.parse(message.toString()));
   } else if (topic === nutrientDispenserStatusTopic) {
-    io.to(stationId).emit('nutrient_dispenser_status', JSON.parse(message.toString()));
+    io.to(stationId).emit(
+      "nutrient_dispenser_status",
+      JSON.parse(message.toString())
+    );
   }
 });
 
-client.on('error', (error) => {
+client.on("error", (error) => {
   console.error(`Error del cliente MQTT: ${error}`);
 });
 
-client.on('close', () => {
-  console.log('Conexión con el broker MQTT cerrada');
+client.on("close", () => {
+  console.log("Conexión con el broker MQTT cerrada");
 });
 
 // Iniciar servidor HTTP en el puerto 8080
--- mqtt-ws-room.js
+++ mqtt-ws-room.js
@@ -1,14 +1,14 @@
-const mqtt = require('mqtt');
-const http = require('http');
-const socketIo = require('socket.io');
+const mqtt = require("mqtt");
+const http = require("http");
+const socketIo = require("socket.io");
 
 // Configuración del broker MQTT de CloudAMQP
-const brokerUrl = "mqtt://toad.rmq.cloudamqp.com";
+const brokerUrl = "mqtt://moose.rmq.cloudamqp.com";
 const brokerPort = 1883;
 
 // Credenciales MQTT
-const username = "jugilxxo:jugilxxo";
-const password = "aqvvDO1Y0hq2iW03wmz08TONcWdov1z0";
+const username = "iqyrnjwl:iqyrnjwl";
+const password = "rQP4_WhDq4ZrynLBTHzbQrD3hG1TrcND";
 
 // Tema MQTT al que suscribirse
 const topicPrefix = "hydrop/668dee66cf7b5b0a30fb22a4/";
@@ -24,27 +24,28 @@ const client = mqtt.connect(brokerUrl, {
 const server = http.createServer();
 const io = socketIo(server, {
   cors: {
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST"]
-  }
+    origin: "http://localhost:3000", // Cambia esto al origen de tu frontend
+    methods: ["GET", "POST"],
+    credentials: true, // Permitir credenciales (cookies, autenticación, etc.)
+  },
 });
 
 // Manejar conexión de cliente Socket.io
-io.on('connection', (socket) => {
-  console.log('Cliente WebSocket conectado');
+io.on("connection", (socket) => {
+  console.log("Cliente WebSocket conectado");
 
-  socket.on('join_station', (stationId) => {
+  socket.on("join_station", (stationId) => {
     console.log(`Cliente unido a la estación: ${stationId}`);
     socket.join(stationId); // Unir al cliente a la sala de la estación
   });
 
-  socket.on('disconnect', () => {
-    console.log('Cliente WebSocket desconectado');
+  socket.on("disconnect", () => {
+    console.log("Cliente WebSocket desconectado");
   });
 });
 
 // Manejar conexión MQTT
-client.on('connect', () => {
+client.on("connect", () => {
   console.log("Conectado al broker MQTT!");
   client.subscribe(`${topicPrefix}#`, (err) => {
     if (err) {
@@ -54,20 +55,20 @@ client.on('connect', () => {
 });
 
 // Manejar mensajes MQTT
-client.on('message', (topic, message) => {
-  const stationId = topic.split('/')[2]; // Extraer el ID de la estación del tema
+client.on("message", (topic, message) => {
+  const stationId = topic.split("/")[2]; // Extraer el ID de la estación del tema
   console.log(`Mensaje recibido en el tema ${topic}: ${message.toString()}`);
 
   // Enviar mensaje a todos los clientes en la sala de la estación
-  io.to(stationId).emit('mqtt_message', message.toString());
+  io.to(stationId).emit("mqtt_message", message.toString());
 });
 
-client.on('error', (error) => {
+client.on("error", (error) => {
   console.error(`Error del cliente MQTT: ${error}`);
 });
 
-client.on('close', () => {
-  console.log('Conexión con el broker MQTT cerrada');
+client.on("close", () => {
+  console.log("Conexión con el broker MQTT cerrada");
 });
 
 // Iniciar servidor HTTP en el puerto 8080
